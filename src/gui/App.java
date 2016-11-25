e2014f6a24c846d90d2ad815da342114a9a9f394
SolidLad <alexsolidlad@gmail.com>
2016-11-25 12:53:54 -0500

    newer layout

--- src/gui/App.java
+++ src/gui/App.java
@@ -16,21 +16,6 @@ public class App extends Application {
         launch(args);
     }
 
-    /**
-     * The main entry point for all JavaFX applications.
-     * The start method is called after the init method has returned,
-     * and after the system is ready for the application to begin running.
-     * <p>
-     * <p>
-     * NOTE: This method is called on the JavaFX Application Thread.
-     * </p>
-     *
-     * @param ps the primary stage for this application, onto which
-     *                     the application scene can be set. The primary stage will be embedded in
-     *                     the browser if the application was launched as an applet.
-     *                     Applications may create other stages, if needed, but they will not be
-     *                     primary stages and will not be embedded in the browser.
-     */
     @Override
     public void start(Stage ps) throws Exception {
         ps.setTitle("Dndood");
